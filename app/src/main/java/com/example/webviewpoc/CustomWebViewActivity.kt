1ff78380b97152fb1a04787f6534b5780015e8fa
Kamlendra Pandey <kamlendra@niro.money>
2022-05-04 19:16:24 +0530

    Initil Commit

---
+++ app/src/main/java/com/example/webviewpoc/CustomWebViewActivity.kt
@@ -0,0 +1,13 @@
+package com.example.webviewpoc
+
+import android.os.Bundle
+
+class CustomWebViewActivity : MainActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onPageFinished() {
+
+    }
+}
---
+++ app/src/main/java/com/example/webviewpoc/SmsListener.java
@@ -0,0 +1,31 @@
+package com.example.webviewpoc;
+
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.provider.Telephony;
+import android.telephony.SmsMessage;
+
+public class SmsListener extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Telephony.Sms.Intents.SMS_RECEIVED_ACTION.equals(intent.getAction())) {
+            for (SmsMessage smsMessage : Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
+                String messageBody = smsMessage.getMessageBody();
+                System.out.println("#SmsListener messageBody >>> " + messageBody);
+                SharedPreferences.Editor editor = context.getSharedPreferences("MyPref", MODE_PRIVATE).edit();
+                editor.putString("messageBody", messageBody);
+                editor.apply();
+            }
+            System.out.println("#SmsListener onReceive 2>>> Outside");
+        }
+        System.out.println("#SmsListener onReceive 1>>> Outside");
+
+    }
+
+
+}
